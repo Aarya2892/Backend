87bc9add1b8f8da5111015b329a7e9894926ca98
Aarya2892 <aaryapatil2892@gamil.com>
2025-12-09 06:35:53 +0530

    started backend course

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+const port= 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/login',(req,res)=>{
+    res.send(`<h1>login successfully`)
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`)
+})
