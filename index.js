7c7f667880de51e1346eb02b7bd5c30c3d712f31
Aarya2892 <aaryapatil2892@gamil.com>
2025-12-09 07:21:04 +0530

    data

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ const express = require('express')
 const app = express()
 const port= 3000;
 
+const userData=[{
+  Id:1,
+  name: 'Denno',
+  course: 'js',
+  payment: 4000
+},
+{
+  Id:2,
+  name: 'james',
+  course: 'react',
+  payment: 7000
+}
+]
+
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
 app.get('/login',(req,res)=>{
     res.send(`<h1>login successfully`)
+})
+
+app.get('/data', (req,res)=>{
+  res.json(userData)
 })
 
 app.listen(port, () => {
